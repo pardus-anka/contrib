9a25e436d07db7ce7e926a9a1faedcc5937df8c3
osmank3 <osmank3@gmail.com>
2013-02-22 23:28:44 +0200

    gnome3 paketleri çomak deposundan çekilip kaynak dosyaları 3.6 sürümüne göre ayarlandı, bağımlılıkları henüz düzenlenmedi, paket adları da değiştirilecek...

---
+++ osmank3/gnome3/base/gnome-bluetooth/actions.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    autotools.autoreconf("-fiv")
+
+    shelltools.system("intltoolize --force --copy --automake")
+
+    autotools.configure("--enable-introspection \
+                        --disable-scrollkeeper \
+                        --disable-desktop-update \
+                        --disable-schemas-install \
+                        --disable-icon-update \
+                        --disable-moblin \
+                        --disable-schemas-compile \
+                        --disable-rpath")
+
+    pisitools.dosed("libtool", "^hardcode_libdir_flag_spec=.*", "hardcode_libdir_flag_spec=\"\"")
+    pisitools.dosed("libtool", "^runpath_var=LD_RUN_PATH", "runpath_var=DIE_RPATH_DIE")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README")
+
